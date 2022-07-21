c1982424827ea662c262c2626cc047c30ddec949
OmarAhmed3012 <omaraboshamaa@gmail.com>
2022-07-21 20:37:21 +0200

    connecting to DB

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
 import cors from 'cors'
+import connectDB from './config/db.js'
+import config from 'config'
 
 const app = express()
+
+connectDB()
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 
 app.use(cors())
+
+const port = config.get('port')
+app.listen(port, () => {
+  console.log(`Server is running on Port ${port}`)
+})
