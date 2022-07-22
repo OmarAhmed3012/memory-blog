e86fdc354a0daadb74695df0a3ea624822c7417a
OmarAhmed3012 <omaraboshamaa@gmail.com>
2022-07-22 18:36:23 +0200

    Get and Add Posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,9 @@
 import express from 'express'
+import { getPost, createPost } from '../controllers/posts.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('works')
-})
+router.get('/', getPost)
+router.post('/', createPost)
 
 export default router
